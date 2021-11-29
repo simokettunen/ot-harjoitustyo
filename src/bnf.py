07fd81fdaad3c69e9d0214ec6db3c34e77c2b592
Simo Kettunen <simo.kettunen@live.fi>
2021-11-30 01:21:23 +0200

    Clean code according to pylint warnings

--- src/bnf.py
+++ src/bnf.py
@@ -1,18 +1,19 @@
 import re
 
 def check_syntax(text):
+    """Check if the given single BNF rule has correct syntax."""
     lines = text.split('\n')
-    
+
     prog = re.compile(r'<[a-z]> ::= ((<[a-z]>|\"\s\")( (<[a-z]>|\"\s\"))*)(\| (<[a-z]>|\"\s\")( (<[a-z]>|\"\s\"))*)*')
 
     for line in lines:
-    
+
         if line == '':
             continue
-    
+
         if not prog.match(line):
             return False
-        
+
     return True
 
 class BNF():
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from ui import UI
 
 def main():
+    """Run the mainloop of the program"""
     app = UI()
     app.loop()
-    
+
 if __name__ == '__main__':
     main()
