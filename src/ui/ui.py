19c56b5795a4a2c81b912a41860bf7efbfe50acc
Simo Kettunen <simo.kettunen@live.fi>
2021-12-07 20:53:21 +0200

    Correct variable name

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -2,21 +2,21 @@ from tkinter import Tk
 from ui.view import EditModeView, StartView
 
 class UI:
-    def __init__(self, database):
+    def __init__(self, service):
         self._root = Tk()
         self._current_view = None
         self._root.geometry('1280x720')
-        self._database = database
+        self._service = service
         
         self._show_start_view()
         
     def _show_edit_mode_view(self):
         self._hide_current_view()
-        self._current_view = EditModeView(self._root, self._database)
+        self._current_view = EditModeView(self._root, self._service)
         
     def _show_start_view(self):
         self._hide_current_view()
-        self._current_view = StartView(self._root, self._show_edit_mode_view, self._database)
+        self._current_view = StartView(self._root, self._show_edit_mode_view, self._service)
     
     def _hide_current_view(self):
         if self._current_view:
